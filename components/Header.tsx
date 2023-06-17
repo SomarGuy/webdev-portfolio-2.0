8c60a1254ca628fd06907a0349e6e652adc55bd4
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-06-17 17:05:56 -0400

    Add SocialIcon to Header with GitHub link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { SocialIcon } from 'react-social-icons'
 
 type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <header>I am a Header</header>
+    <header>
+      <div>
+        {/* Social Icons */}
+        <SocialIcon url="https://github.com/SomarGuy" />
+      </div>
+    </header>
   )
 }
