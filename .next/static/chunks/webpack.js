6a2af72a66aa7f771d1e739f2332172a24c255e2
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-06-20 17:48:04 -0400

    Update typewriter text in Hero component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c45fd35374bbc189"; }
+/******/ 		__webpack_require__.h = function() { return "47dcbddf3e11d7df"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Cursor, useTypewriter } from 'react-simple-typewriter'
+
 
 type Props = {}
 
 export default function Hero({}: Props) {
-  return (
-    <div>Hero</div>
-  )
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi!, My Name's Diogo Ramos",
+      "PassionateDeveloper.tsx",
+      "Coffee-Is-My-Programming-Fuel",
+  ],
+  loop: true,
+  delaySpeed: 2500,
+  });
+  return <div>
+    <p>{text}</p>
+  </div>;
 }
