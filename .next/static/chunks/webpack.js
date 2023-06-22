be9e1acca70a5b93393a2dc3f83bd8d1e0e2d289
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-06-22 15:50:02 -0400

    Extend BackgroundCircles with additional styled divs

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e64036e679d307c7"; }
+/******/ 		__webpack_require__.h = function() { return "7100b6a94c1497d6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -6,12 +6,15 @@ type Props = {}
 function BackgroundCircles({}: Props) {
   return (
     <div className='relative flex justify-center items-center'>
-      <div className='border #3333333 rounded-full h-[200px] w-[200px] mt-52
+      <div className='absolute border [#333333] rounded-full h-[200px] w-[200px] mt-52
       animate-ping' />
-      <div />
-      <div />
-      <div />
-      <div />
+      <div className='absolute border-[#333333] rounded-full h-[300px] w-[300px] mt-52
+      animate-ping' />
+      <div className='absolute border-[#333333] rounded-full h-[500px] w-[500px] mt-52
+      animate-ping' />
+      <div className='rounded-full border border-[#7DE2D1] opacity-20 h-[650px] w-[650px]
+      absolute mt-52 animate-pulse'/>
+      <div className='rounded-full border border-[#333333] h-[800px] w-[800px] absolute mt-52'/>
     </div>
   )
 }
