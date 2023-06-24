e295f387ccc534deba72847e30584cffcdfb21f0
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-06-23 23:34:52 -0400

    Add and style 'About' heading in 'About' component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "740f145201239b1f"; }
+/******/ 		__webpack_require__.h = function() { return "6c680d73d34729cd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
