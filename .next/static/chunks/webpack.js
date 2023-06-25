f93f6ccaee6b4a5c154627e095b56fd5b794a581
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-06-25 15:59:34 -0400

    Add job title, company, tech stack, period, and summary to ExperienceCard

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3f7a13e867c57bfc"; }
+/******/ 		__webpack_require__.h = function() { return "3f3a2adf022421c1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -17,6 +17,30 @@ function ExperienceCard({}: Props) {
     object-cover object-center'
     src="https://media.licdn.com/dms/image/C4E0BAQH3nUDO57s78A/company-logo_200_200/0/1531936161449?e=1695859200&v=beta&t=N7WHifromNrxOyf5uY96n3ck3ZWyjlk2ndvBV-JgWVU" 
     alt="" />
+
+    <div className='px-0 md:px-10'>
+      <h4 className='text-4xl font-light'>Full-Stack Web Developer</h4>
+      <p className='font-bold text-2xl mt-1'>Freelance</p>
+      <div className='flex space-x-2 my-2'>
+        <img className='h-10 w-10 rounded-full' src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png" alt="" />
+        {/* Tech used */}
+        {/* Tech used */}
+        {/* Tech used */}
+        {/* Tech used */}
+        {/* Tech used */}
+        {/* Tech used */}
+        {/* Tech used */}
+      </div>
+      <p>Started work... - Ended...</p>
+
+      <ul className='list-disc space-y-4 ml-5 text-lg'>
+        <li>Summary points</li>
+        <li>Summary points</li>
+        <li>Summary points</li>
+        <li>Summary points</li>
+        <li>Summary points</li>
+      </ul>
+    </div>
   </article>
 }
 
