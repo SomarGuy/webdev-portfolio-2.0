14d8708158e0b325690c80969d5511b00cbf0566
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-06-25 15:52:29 -0400

    Add framer-motion animation to image in ExperienceCard component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "494bbb701596e796"; }
+/******/ 		__webpack_require__.h = function() { return "3f7a13e867c57bfc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 type Props = {}
 
 function ExperienceCard({}: Props) {
   return <article>
-    <img 
+    <motion.img
+    initial={{
+      y: -100,
+      opacity: 0,
+    }} 
+    transition={{ duration: 1.2 }}
+    whileInView={{ opacity: 1, y: 0}}
+    viewport={{ once: true }}
     className='w-32 h-32 rounded-full md:rounded-full xl:w-[200px] xl:h-[200px]
     object-cover object-center'
-    src="https://media.licdn.com/dms/image/C4E0BAQH3nUDO57s78A/company-logo_200_200/0/1531936161449?e=1695859200&v=beta&t=N7WHifromNrxOyf5uY96n3ck3ZWyjlk2ndvBV-JgWVU" alt="" />
+    src="https://media.licdn.com/dms/image/C4E0BAQH3nUDO57s78A/company-logo_200_200/0/1531936161449?e=1695859200&v=beta&t=N7WHifromNrxOyf5uY96n3ck3ZWyjlk2ndvBV-JgWVU" 
+    alt="" />
   </article>
 }
 
