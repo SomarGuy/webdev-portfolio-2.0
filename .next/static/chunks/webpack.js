48e2b08b86b81593e9ecdf3beb1d3eeaffcc3723
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-06-24 23:29:01 -0400

    Added fade-in animation to About component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3d859f45b782b3f5"; }
+/******/ 		__webpack_require__.h = function() { return "1767e880584c0bd2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/About.tsx
+++ components/About.tsx
@@ -5,7 +5,11 @@ type Props = {}
 
 function About({}: Props) {
   return (
-    <div className='flex flex-col relative h-screen text-center md:text-left
+    <motion.div
+    initial={{ opacity: 0 }}
+    whileInView={{ opacity: 1 }}
+    transition={{ duration: 1.5 }}
+    className='flex flex-col relative h-screen text-center md:text-left
     md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
         About
@@ -29,7 +33,7 @@ function About({}: Props) {
       <h4 className='text-4xl font-semibold'>My <span className='underline decoration-[#7DE2D1]/50'>Journey</span> So Far</h4>
       <p>Hey! I'm Diogo Ramos 🐋, a Full-Stack Web Developer based in Toronto with a solid foundation in computer science and 5 years of experience in web development. Graduated from Lighthouse Labs' Full Stack Web Development Bootcamp and Western Technical Commercial School's Specialist High Skills Major (SHSM) program in Robotics, I've acquired an in-depth knowledge and hands-on experience with modern web technologies. My technical expertise ranges from programming languages such as TypeScript, JavaScript, Arduino, Java, Ruby, Python, to web development technologies like HTML, CSS, PHP, React, Node.js, Express, Vite, Next.js, and Tailwind CSS. I also have experience with Unit Testing using tools like Jest, Mocha, and Cypress, and Database Management with PostgreSQL, Ruby-on-Rails, and SQL. Always eager to leverage my technical expertise and problem-solving abilities to contribute to impactful missions. I thrive in fast-paced, rapidly changing environments and believe in building software that offers great user experience! ⭐</p>
      </div>
-    </div>
+    </motion.div>
   )
 }
 
