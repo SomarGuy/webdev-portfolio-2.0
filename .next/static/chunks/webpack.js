e62de5831f0c8a150fa2cfd7f095e1d610b876ea
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-06-24 23:48:11 -0400

    Add image and initial layout to ExperienceCard component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9b8d756c0541a40d"; }
+/******/ 		__webpack_require__.h = function() { return "494bbb701596e796"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -3,9 +3,12 @@ import React from 'react'
 type Props = {}
 
 function ExperienceCard({}: Props) {
-  return (
-    <div>ExperienceCard</div>
-  )
+  return <article>
+    <img 
+    className='w-32 h-32 rounded-full md:rounded-full xl:w-[200px] xl:h-[200px]
+    object-cover object-center'
+    src="https://media.licdn.com/dms/image/C4E0BAQH3nUDO57s78A/company-logo_200_200/0/1531936161449?e=1695859200&v=beta&t=N7WHifromNrxOyf5uY96n3ck3ZWyjlk2ndvBV-JgWVU" alt="" />
+  </article>
 }
 
 export default ExperienceCard
