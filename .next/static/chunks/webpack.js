594d187e3e7b6be840e238283e2ec3ee77d4cf60
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-06-29 11:12:35 -0400

    Add initial styling to Projects component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dd53462378fc0ff6"; }
+/******/ 		__webpack_require__.h = function() { return "5cc73c98b6a4c0ba"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,7 +4,9 @@ type Props = {}
 
 function Projects({}: Props) {
   return (
-    <div className='h-screen'>Projects</div>
+    <div className='h-screen relative'>
+      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Projects</h3>
+    </div>
   )
 }
 
