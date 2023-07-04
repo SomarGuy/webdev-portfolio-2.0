d41baedd42a881790f4a95047283693eb93cce96
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-07-03 23:36:07 -0400

    add styling for contact header

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "046717578c9ba5e0"; }
+/******/ 		__webpack_require__.h = function() { return "0910f2f184b3916c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
