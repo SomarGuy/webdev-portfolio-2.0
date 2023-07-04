a209c857e143d6156dade33d3a20d5639ce768e8
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-07-03 23:29:15 -0400

    simple opacity animations

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d8ab14fd4dbf9c65"; }
+/******/ 		__webpack_require__.h = function() { return "fd9145818e4770e4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,7 +5,10 @@ type Props = {}
 function Projects({}: Props) {
   const projects = [1, 2, 3, 4, 5];
   return (
-    <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
+    <motion.div
+    initial={{ opacity: 0 }}
+    whileInView={{ opacity: 1 }}
+    transition={{ duration: 1.5 }} className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
         Projects
       </h3>
@@ -33,7 +36,7 @@ function Projects({}: Props) {
       </div>
 
       <div className='w-full absolute top-[30%] bg-[#7DE2D1]/10 left-0 h-[500px] -skew-y-12' />
-    </div>
+    </motion.div>
   );
 }
 
