0b2b10b5437e79996d81bb701e1e0bdf967b9157
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-07-03 23:33:22 -0400

    create contact me and add to index

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d8add6cd9aa25c97"; }
+/******/ 		__webpack_require__.h = function() { return "046717578c9ba5e0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import About from '@/components/About'
 import WorkExperience from '@/components/WorkExperience'
 import Skills from '@/components/Skills'
 import Projects from '@/components/Projects'
+import ContactMe from '@/components/ContactMe'
 
 const Home: NextPage = () => {
   return (
@@ -41,7 +42,10 @@ const Home: NextPage = () => {
       </section>
 
       {/* Contact Me */}
-
+      <section id='contact'
+      className='snap-start'>
+        <ContactMe />
+      </section>
     </div>
   )
 }
