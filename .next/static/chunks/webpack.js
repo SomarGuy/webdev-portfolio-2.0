926e823d6f168a816e58c24d64b17e85dd25bc62
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-07-03 23:27:55 -0400

    import motion

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "52418a6d9b574993"; }
+/******/ 		__webpack_require__.h = function() { return "d8ab14fd4dbf9c65"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
