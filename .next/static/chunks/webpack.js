b9f5c8c357547715d41a6a0833878a01dbd429c0
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-07-12 15:37:00 -0400

    add placeholder text for input areas

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2f1e00bb3af5611d"; }
+/******/ 		__webpack_require__.h = function() { return "c0986bf877f5fa89"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
